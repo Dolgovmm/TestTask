7d2387d4601488a2d22a5a35c11f7eebee5e12f7
dolgowmm <dolgowmm@gmail.com>
2017-05-22 21:55:19 +0700

    add interface Repository.java

---
+++ src/main/java/ru/dolgov/webservice/repository/Repository.java
@@ -0,0 +1,21 @@
+package ru.dolgov.webservice.repository;
+
+import ru.dolgov.webservice.entity.Contact;
+
+import java.util.List;
+
+/**
+ * Created by Михалыч on 22.05.2017.
+ */
+public interface Repository {
+
+    public long add(Contact entity);
+
+    public Contact getById(long id);
+
+    public List<Contact> getAll();
+
+    public long update(Contact entity);
+
+    public long delete(long id);
+}
