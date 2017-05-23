23c8c943864329e199b3bc15690d205dede16e0d
dolgowmm <dolgowmm@gmail.com>
2017-05-23 19:21:56 +0700

    add RepositoryImpl class with default implement methods

---
+++ src/main/java/ru/dolgov/webservice/repository/RepositoryImpl.java
@@ -0,0 +1,38 @@
+package ru.dolgov.webservice.repository;
+
+import ru.dolgov.webservice.entity.Contact;
+
+import java.util.List;
+
+/**
+ * Created by Михалыч on 23.05.2017.
+ */
+public class RepositoryImpl implements Repository {
+
+    
+
+    @Override
+    public long add(Contact entity) {
+        return 0;
+    }
+
+    @Override
+    public Contact getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Contact> getAll() {
+        return null;
+    }
+
+    @Override
+    public long update(Contact entity) {
+        return 0;
+    }
+
+    @Override
+    public long delete(long id) {
+        return 0;
+    }
+}
