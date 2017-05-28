214293e855789a07b0127b8aa2c7d549fbb0c4a7
dolgowmm <dolgowmm@gmail.com>
2017-05-28 10:29:47 +0700

    add Contact.java

--- client/src/main/java/ru/dolgov/restclient/controller/RootSceneController.java
+++ client/src/main/java/ru/dolgov/restclient/controller/RootSceneController.java
@@ -4,5 +4,5 @@ package ru.dolgov.restclient.controller;
  * Created by Михалыч on 28.05.2017.
  */
 public class RootSceneController {
-    
+
 }
---
+++ client/src/main/java/ru/dolgov/restclient/entity/Contact.java
@@ -0,0 +1,88 @@
+package ru.dolgov.webservice.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @author M.Dolgov
+ * @date 22.05.2017
+ */
+@Entity
+@Table(name = "contacts")
+public class Contact implements Serializable{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "firstname", length = 255, nullable = false, unique = true)
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephoneNumber(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        if (this == obj) return true;
+        Contact contact = (Contact) obj;
+        if (this.id == contact.id && this.firstName.equals(contact.getFirstName()) &&
+                this.lastName.equals(contact.getLastName()) && this.getTelephone().equals(contact.getTelephone()) &&
+                this.email.equals(contact.getEmail())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [id= " + id + ", firstName= " + firstName + ", lastName= " + lastName + ", email= " + email
+                + ", telephone= " + telephone + "]";
+    }
+}
