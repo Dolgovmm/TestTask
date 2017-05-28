9a6fd4957884b8a312ac48477c3f81b41129e8a9
dolgowmm <dolgowmm@gmail.com>
2017-05-28 08:43:16 +0700

    add elements to rootScene.fxml

--- client/src/main/java/ru/dolgov/restclient/main/MainApp.java
+++ client/src/main/java/ru/dolgov/restclient/main/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
         String fxmlFile = "/fxml/rootScene.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("REST client");
         stage.setScene(new Scene(root));
         stage.show();
     }
